a6cb2f890ea9175c47d82d97e924c3c1bc2e839d
vdekh <vicdekh@gmail.com>
2018-01-19 00:23:20 +0300

    Migrated to Aura Router

--- public/index.php
+++ public/index.php
@@ -1,5 +1,9 @@
 <?php
 
+use App\Http\Action;
+use Framework\Http\ActionResolver;
+use Framework\Http\Router\AuraRouterAdapter;
+use Framework\Http\Router\Exception\RequestNotMatchedException;
 use Zend\Diactoros\Response\HtmlResponse;
 use Zend\Diactoros\Response\SapiEmitter;
 use Zend\Diactoros\ServerRequestFactory;
@@ -9,14 +13,34 @@ require 'vendor/autoload.php';
 
 ### Initialization
 
+$aura = new Aura\Router\RouterContainer();
+$routes = $aura->getMap();
+
+$routes->get('home', '/', Action\HelloAction::class);
+$routes->get('about', '/about', Action\AboutAction::class);
+$routes->get('blog', '/blog', Action\Blog\IndexAction::class);
+$routes->get('blog_show', '/blog/{id}', Action\Blog\ShowAction::class)->tokens(['id' => '\d+']);
+
+$router = new AuraRouterAdapter($aura);
+$resolver = new ActionResolver();
+
+### Running
+
 $request = ServerRequestFactory::fromGlobals();
+try {
+    $result = $router->match($request);
+    foreach ($result->getAttributes() as $attribute => $value) {
+        $request = $request->withAttribute($attribute, $value);
+    }
+    $action = $resolver->resolve($result->getHandler());
+    $response = $action($request);
+} catch (RequestNotMatchedException $e){
+    $response = new HtmlResponse('Undefined page', 404);
+}
 
-### Action
+### Postprocessing
 
-$name = $request->getQueryParams()['name'] ?? 'Guest';
-
-$response = (new HtmlResponse('Hello, ' . $name . '!'))
-    ->withHeader('X-Developer', 'vdekh');
+$response = $response->withHeader('X-Developer', 'vdekh');
 
 ### Sending
 
