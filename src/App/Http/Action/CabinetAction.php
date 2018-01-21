e7abfa1fbf4a93d60ec300ad36be1197f793fae8
vdekh <vicdekh@gmail.com>
2018-01-21 14:13:06 +0300

    Added CabinetAction

---
+++ src/App/Http/Action/CabinetAction.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Action;
+
+use App\Http\Middleware\BasicAuthMiddleware;
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response\HtmlResponse;
+
+class CabinetAction
+{
+    public function __invoke(ServerRequestInterface $request)
+    {
+        $username = $request->getAttribute(BasicAuthMiddleware::ATTRIBUTE);
+        return new HtmlResponse('I am logged in as ' . $username);
+    }
+}
